8dfd237297f8f743272a6afaf361636367013d9c
rebreather <yds04116@gmail.com>
2021-10-11 18:16:16 +0900

    로그인을 요청하는 fetch 생성

--- appback/src/public/js/home/login.js
+++ appback/src/public/js/home/login.js
@@ -1,5 +1,7 @@
 ﻿"use strict";
 
+const { post } = require("../../../routes/home");
+
 const id = document.querySelector("#id");
 const psword = document.querySelector("#psw");
 const loginbutton = document.querySelector("button");
@@ -9,9 +11,16 @@ loginbutton.addEventListener("click", login);
 
 function login() {
     const req = {
-        id: id.value,
+        id: id.value, //login.ejs 파일 안에 있는 id값에 const 변수에 담긴 객체를 보여줌.
         psw: psword.value,
     };   
-    console.log(req);
+
+    fetch("/login", {
+        method: "POST",
+        headers: { //내가 전달하는 데이터가 json이라는걸 헤더로 표현 가능
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req)
+    });
 
 }
