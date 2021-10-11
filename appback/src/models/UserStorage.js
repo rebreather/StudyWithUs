39e7ca61b58132e083e3dd047baff9fd6923e4ae
rebreather <yds04116@gmail.com>
2021-10-12 03:50:26 +0900

    UserStorage 모델 구현

---
+++ appback/src/models/UserStorage.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+class UserStorage {
+    static #users = { //const 같은 선언자 필요없음
+        id: ["gy","gyy","lgy"],
+        psword: ["111","1234","1231"],
+        name: ["우리", "연", "김"]
+    };
+
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers, field) => {
+                         //fields 배열의 초기값이 newUsers에 들어가고 그 다음 변수가 field에 들어감
+            if(users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        }, {});
+        //console.log(newUsers);
+        return newUsers;
+    }
+
+}
+
+module.exports = UserStorage;
--- appback/src/routes/home/home.ctrl.js
+++ appback/src/routes/home/home.ctrl.js
@@ -1,5 +1,7 @@
 ﻿"use strict";
 
+const UserStorage = require("../../models/UserStorage");
+
 const output = {
     hello: (req, res) => {
         res.render("home/index");
@@ -9,29 +11,29 @@ const output = {
     },
 };
 
-const users = {
-    id: ["gy","gyy","lgy"],
-    psword: ["111","1234","1231"],
-};
+
 
 const process = {
     login: (req, res) => {
         const id = req.body.id;
         const psword = req.body.psw;
+        //const userStorage = new UserStorage();
 
+        const users = UserStorage.getUsers("id","psword");
+        //console.log(UserStorage.users);
+
+        const response = {};
         if (users.id.includes(id)) {
             const idx = users.id.indexOf(id);
             if (users.psword[idx] === psword) {
-                return res.json({
-                    success: true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
 
-        return res.json({
-            success: false,
-            msg: "로그인에 실패했습니다.",
-        });
+        response.success = false;
+        response.msg = "로그인에 실패했습니다."
+        return res.json(response);
     },
 }
 
