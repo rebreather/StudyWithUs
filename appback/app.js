983534cd4473490b5cc9eaf78f5259b8da5de9a4
rebreather <yds04116@gmail.com>
2021-10-11 17:47:33 +0900

    front단 js 파일 만들기

--- appback/app.js
+++ appback/app.js
@@ -6,6 +6,7 @@ const home = require("./src/routes/home/index.js");
 app.set("views", "./src/views"); // 템플릿이 있는 디렉토리
 app.set("view engine", "ejs"); //사용할 템플릿 엔진
 
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); //use 메소드 -> 미들웨어를 등록하는 메소드
  //루트경로로 들어오면 home으로 보내준다는 의미, home 경로는 위에 명시 되어있음.
