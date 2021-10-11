bce2ec953ff6838c3dd18cb6c07ef925dd7b5ab2
rebreather <yds04116@gmail.com>
2021-10-11 18:06:20 +0900

    DOM을 이용하여 HTML객체 저장

--- appback/src/public/js/home/login.js
+++ appback/src/public/js/home/login.js
@@ -1,5 +1,17 @@
 ﻿"use strict";
 
+const id = document.querySelector("#id");
+const psword = document.querySelector("#psw");
+const loginbutton = document.querySelector("button");
 
-console.log("hello");
-console.log("bye");
+
+loginbutton.addEventListener("click", login);
+
+function login() {
+    const req = {
+        id: id.value,
+        psw: psword.value,
+    };   
+    console.log(req);
+
+}
